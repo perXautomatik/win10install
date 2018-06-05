6cc755c42eb053ca894816e3c7306119ea331c21
askvittorio <askvittorio@gmail.com>
2018-06-05 15:58:36 +0200

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -1,19 +1,6 @@
-#After extracting Boxstarter.1.0.0.zip on MYCOMPUTER
-.\setup.bat
-Import-Module $env:appdata\boxstarter\Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1
-#Create minimal nuspec and chocolateyInstall
-New-BoxstarterPackage MyPackage
-#Edit Install script to customize your environment
-Notepad Join-Path $Boxstarter.LocalRepo "tools\ChocolateyInstall.ps1"
-#Pack nupkg
-Invoke-BoxstarterBuild MyPackage
-
-#share Repo
-Set-BoxstarterShare
-#Or Copy to thumb drive G
-Copy-Item $Boxstarter.BaseDir G:\ -Recurse
-
-#Logon to your bare Windows install
-\\MYCOMPUTER\Boxstarter\Boxstarter Mypackage
-
-#Enter password when prompted and come back later to find all your apps installed
+$GistUrl = Read-Host -Prompt 'Input your Github raw url'
+$confirmation = Read-Host "Your url was '$GistUrl'. Is that correct? (Y/N)"
+if ($confirmation -eq 'y') {
+  # proceed
+  START https://boxstarter.org/package/nr/url?$GistUrl
+}
